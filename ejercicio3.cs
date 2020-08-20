8358ee3b8c55a88d364208c96ec52bc9e98f7fd1
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-08-19 19:06:27 -0600

    Corregi el ejercicio 3

--- ejercicio3.cs
+++ ejercicio3.cs
@@ -6,12 +6,14 @@ namespace ejercicio3
     {
         static void Main(string[] args)
         {
-            double dolares, colones;
+            double dolares, colones, colon;
 
             Console.WriteLine("Digite la cantidad de dolares que quiere cambiar a colones: ");
             dolares = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite cuanto vale el colon: ");
+            colon = Convert.ToDouble(Console.ReadLine());
 
-            colones = dolares / 0.1142;
+            colones = dolares / colon;
 
             Console.WriteLine("esta es la conversion de {0:C2} dolares a colones {1:C2}", dolares, colones);
         }
